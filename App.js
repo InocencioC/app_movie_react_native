f3fb1dbeb92fb012a1c69fbfbe5848f62b93458c
Inocencio Codeiro <cordeiroturorias@gmail.com>
2022-02-25 14:46:14 +0100

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Olá Mundo</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   
   },
 });
