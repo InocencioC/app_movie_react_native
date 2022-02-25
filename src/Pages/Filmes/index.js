cfe7f5243c9bc9aa2b07e3bc33177fb377083690
Inocencio Codeiro <cordeiroturorias@gmail.com>
2022-02-25 16:01:45 +0100

    Capa do filme e titulos alinhados

--- src/Pages/Filmes/index.js
+++ src/Pages/Filmes/index.js
@@ -1,12 +1,15 @@
 import React, {Component} from "react";
-import { View, Text,Image, StyleSheet} from "react-native";
+import { View, Text,Image, StyleSheet, TouchableOpacity} from "react-native";
 
 export default class Filme extends Component {
     render(){
         return(
             <View>
                 <Text style={styles.nomeFilme}>{this.props.data.nome}</Text>
+                <TouchableOpacity onPress={() => alert(this.props.data.nome)}>
                 <Image style={styles.capaFilme} source={{uri: this.props.data.foto}}/>
+                </TouchableOpacity>
+             
             </View>
         )
     }
@@ -18,10 +21,19 @@ const styles =  StyleSheet.create({
     nomeFilme:{
         color:'white',
         fontSize: 18,
-        fontWeight: 'bold' 
+        fontWeight: 'bold',
+        justifyContent: 'center',
+        alignSelf:'center',
+        marginTop: 30,
+        marginBottom: 20
+        
     },
     capaFilme:{
-        width: 120,
-        height: 150,
+        width: 250,
+        height: 250,
+        borderRadius: 30,
+        alignSelf: 'center'
+
+
     }
 });
